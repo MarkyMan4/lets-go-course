0e3dafb4fdab260cd4dfc2b32ca0f55100825249
MarkyMan4 <markus.hutnik@gmail.com>
2024-03-06 23:30:24 -0600

    query parameters

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -14,7 +16,13 @@ func home(w http.ResponseWriter, r *http.Request) {
 }
 
 func snippetView(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("display a snippet..."))
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "displaying snipped with ID %d...", id)
 }
 
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
